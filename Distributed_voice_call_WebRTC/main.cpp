8335ede30267e17db1a80d19f30b82d978239f71
Fatemeh Shakiba Baharvand <shakibabaharvand2000@gmail.com>
2024-10-07 14:05:58 +0330

    init()

---
+++ Distributed_voice_call_WebRTC/main.cpp
@@ -0,0 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("Distributed_voice_call_WebRTC", "Main");
+
+    return app.exec();
+}
