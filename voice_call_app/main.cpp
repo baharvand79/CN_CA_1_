2a77faa5e043f27cb2c7935140546a3b7cd3c9e6
Fatemeh Shakiba Baharvand <shakibabaharvand2000@gmail.com>
2024-10-15 22:59:16 +0330

    dummyTest(AudioInput)

--- voice_call_app/main.cpp
+++ voice_call_app/main.cpp
@@ -2,21 +2,36 @@
 #include <QQmlApplicationEngine>
 
 
+//int main(int argc, char *argv[])
+//{
+//#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+//    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+//#endif
+//    QGuiApplication app(argc, argv);
+
+//    QQmlApplicationEngine engine;
+//    const QUrl url(QStringLiteral("qrc:/main.qml"));
+//    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+//                     &app, [url](QObject *obj, const QUrl &objUrl) {
+//        if (!obj && url == objUrl)
+//            QCoreApplication::exit(-1);
+//    }, Qt::QueuedConnection);
+//    engine.load(url);
+
+//    return app.exec();
+//}
+
+#include <QCoreApplication>
+#include "audioinput.h"
+
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-    QGuiApplication app(argc, argv);
+    // Initialize Qt application
+    QCoreApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    // Create an instance of AudioInput
+    AudioInput audioInput;
 
+    // The event loop will now run, allowing the audio capture process to continue.
     return app.exec();
 }
